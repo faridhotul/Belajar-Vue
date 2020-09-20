e57d69cda1606fb96fe36a953e48250e07438e9f
Faridhotul <faridhotul.khasanah@gmail.com>
2020-09-20 10:58:10 +0700

    update per 20 September 2020

--- apps.js
+++ apps.js
@@ -1,27 +1,10 @@
 var app = new Vue ({
     el:'#app',
     data: {
-        a:3,
-        b:5
-    }, 
-
-    computed: {
-        jumlah  : function(){
-            return this.a + this.b
-        },
-
-        kurang  : function(){
-            return this.a - this.b
-        },
-
-        kali  : function(){
-            return this.a * this.b
-        },
-
-        bagi  : function(){
-            return this.a / this.b
-        },
+        imageSrc :'me.jpg',
     }
+
+    
     /*data:{
         text   : 'Halo Farid Kuat!',
         angka1 : 100,
@@ -56,5 +39,67 @@ var app = new Vue ({
     }*/
 
     //computed - methode
-    
+    /*computed: {
+        jumlah  : function(){
+            return this.a + this.b
+        },
+
+        kurang  : function(){
+            return this.a - this.b
+        },
+
+        kali  : function(){
+            return this.a * this.b
+        },
+
+        bagi  : function(){
+            return this.a / this.b
+        },
+    }*/
+
+    /*methods  : {
+        diproses(){
+            this.jumlah = this.a + this.b,
+            this.kurang = this.a - this.b,
+            this.kali = this.a * this.b,
+            this.bagi = this.a / this.b
+        }
+    }*/
+    /*
+    watch:{
+        isi : function(){
+            this.keterangan = 'sedang mengetik....'
+        }
+    }*/
+
+    //Watch dengan methode
+    /*watch : {
+        isi : 'ketik'
+    },
+    methods:{
+        ketik : function(){
+        if(this.isi == 'cinta'){
+            this.keterangan ='sayang'
+        } else{
+            this.keterangan = 'sedang mengetik...'
+            }
+        }
+    }*/
+    //watch dengan login
+    /*watch:{
+        isi2: 'ketik'
+    },
+    methods:{
+        ketik : function(){
+            if (this.isi2 == this.isi){
+            this.keterangan = '<i class="text-success">password sama silahkan lanjutkan</i>'
+            this.tampil = true;
+        }else{
+            this.keterangan = '<i class ="text-danger">password tidak sama silahkan ulangi</i>'
+            this.tampil = false;
+
+        }
+    }
+}*/
+  
 });
