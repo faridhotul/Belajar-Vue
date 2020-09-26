8e06fa6a6a772daead8b44fc2bb14ff22de674ce
Faridhotul <faridhotul.khasanah@gmail.com>
2020-09-26 19:51:54 +0700

    commit 26 sept 2020

--- apps.js
+++ apps.js
@@ -1,9 +1,153 @@
+/*Vue.component('daftar', {
+    template: '<div><siswa v-for="s in daftarsiswa">{{s.nama}} kelas {{s.kelas}} </siswa></div>',
+    data: function(){
+        return {
+            daftarsiswa:[
+                {nama : 'Farid', kelas : '12'},
+                {nama : 'Dhotul', kelas : '12'},
+                {nama : 'Khasanah', kelas : '12'}
+            ]
+        }
+    }
+   
+});
+
+Vue.component('siswa' , {
+    template: '<ul><li><slot></slot></li></ul>'
+});*/
+
+//props
+/*
+Vue.component('karya' , {
+    props : ['judul', 'isi'],
+    template : '<div><h2>{{judul}}</h2><p>{{isi}}</p></div>'
+});*/
+
+//props lv 2
+/*
+Vue.component('tes', {
+    props : ['nilai', 'nilai2'],
+    template : '<div><b>{{nilai}}</b> harus <b>{{nilai2}}</b></div>'
+})*/
+
+
+Vue.component ('nama', {
+    template: " <input type='text' placeholder = 'Masukkan nama Anda' @blur ='validasi'> </input> ",
+    methods : {
+        validasi (){
+            this.$emit('applied')
+        }
+    }
+});
 var app = new Vue ({
     el:'#app',
     data: {
-        isi:'',
-        kendaraan: ['mobil','motor','sepeda']
+        valid : false
     },
+    methods : {
+        validasi() {
+            this.valid = true;
+        }
+    }
+
+
+
+
+
+    //capitalize
+    /*
+    data : {
+        pesan : 'halo Farid sukses halo'
+    },
+    filters :{
+        capitalize: function (value){
+            if (!value) return ''
+            value = value.toString()
+            return value.charAt(0) .toUpperCase() + value.slice(1)
+        }
+    }*/
+
+    //flipword
+    /*
+    data : {
+        pesan : 'halo Farid sukses'
+    },
+    filters :{
+        terbalik(value){
+            if (!value) return ''
+            value = value.toString()
+            return value.split('') .reverse().join('')
+        },
+        terbalik2 (value){
+            if (!value) return ''
+            value = value.toString()
+            return value.split(' ') .reverse().join(' ')
+        },
+    }*/
+
+
+    //flip word
+    /*
+    data : {
+        pesan : 'halo Farid sukses'
+    },
+    filters :{
+        kapital(value){
+            if (!value) return ''
+            value = value.toString()
+            return value.toUpperCase()
+        },
+        kecil (value){
+            if (!value) return ''
+            value = value.toString()
+            return value.toLowerCase()
+        },
+    }*/
+
+    // array computed property
+    /*
+    data :{
+        cari:'',
+        kendaraan :[
+            {jenis : 'mobil', merk : 'honda' , warna : 'merah'},
+            {jenis : 'motor', merk : 'honda' , warna : 'hitam'},
+            {jenis : 'sepeda', merk : 'phoenix' , warna : 'biru'},
+        ]
+    },
+    computed:{
+        filterkendaraan: function(){
+            return this.kendaraan.filter((kd)=>{
+                return kd.merk.match(this.cari)
+            });
+        }
+    }*/
+  
+
+
+
+//array ft computed property
+    /*  data: {
+            barang: [
+                {jenis : 'mie rebus', stok:true},
+                {jenis : 'mie goreng', stok:true},
+                {jenis : 'mie gacoan', stok:true},
+                {jenis : 'yamie panda', stok:false},
+                {jenis : 'mie nyinyr', stok:false},
+                {jenis : 'mie apah', stok:false}
+        ]
+    },
+    computed:{
+        barangAda(){
+            return this.barang.filter(b => b.stok)
+        },
+        barangHabis(){
+            return this.barang.filter(b => !b.stok)
+        },
+    }*/
+
+
+    // menghapus array
+    /*
     methods:{
         tambah: function(){
             this.kendaraan.push(this.isi)
@@ -12,7 +156,7 @@ var app = new Vue ({
         hapus(index){
             this.kendaraan.splice(index,1)
         }
-    }
+    }*/
         
 
     
